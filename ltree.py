16bd5f2699a2a7845a532a0ccb560eeb2dcb5f88
ax-rwnd <axel-sundbom@hotmail.com>
2017-06-16 02:29:10 +0200

    Fixes oversight

--- ltree.py
+++ ltree.py
@@ -22,16 +22,17 @@ class LTree:
 
 		syn = []
 		for s in axiom:
-			if not s in env:
-				raise ValueError("Undefined variable '"+s+"'!")
-			elif (s==self.Sym.Left.rep):
+			if (s==self.Sym.Left.rep):
 				syn += [self.Sym.Left.func]
 			elif (s==self.Sym.Right.rep):
 				syn += [self.Sym.Right.func]
 			elif (s==self.Sym.Move.rep):
 				syn += [self.Sym.Move.func]
 			else:
-				syn += self.parse(env[s], env, depth-1)
+				if not s in env:
+					raise ValueError("Undefined variable '"+s+"'!")
+				else:
+					syn += self.parse(env[s], env, depth-1)
 		return syn
 	
 	def exec(self, turtle, syn):
